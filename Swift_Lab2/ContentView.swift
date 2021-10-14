e5b9df5d39c4f8bc066005ab507c09b5dcd83398
BrotskyS <sergiypik.work@gmail.com>
2021-10-14 11:45:33 +0300

    Initial Commit

---
+++ Swift_Lab2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_Lab2
+//
+//  Created by user on 14.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_Lab2/Swift_Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Lab2App.swift
+//  Swift_Lab2
+//
+//  Created by user on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
